b83332bfc0464205768bcff6be5b9b32619714fe
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-10-21 21:46:51 +0000

    started

--- Main.java
+++ Main.java
@@ -4,6 +4,30 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // Problem 1
+
+    for (int i = 1; i <= 25; i += 2)
+    {
+      System.out.println(i + " ");
+    }
+
+    //Problem 2
+    System.out.println("Problem 2: ");
+    int counter = 0;
+    for (int i = 17; i <= 73; i++)
+    {
+      
+      if (counter % 10 == 9)
+      {
+      System.out.println(i + " ");
+      }
+      else
+      {
+        System.out.print(i + " ");
+      }
+      counter++;
+    }
+
+
   }
 }
