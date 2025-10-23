22c626f226b6f7a21877ea072c55198ad17bfcba
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-10-23 21:29:48 +0000

    finished

--- Main.java
+++ Main.java
@@ -28,6 +28,63 @@ public class Main
       counter++;
     }
 
+    //problem 3
+    System.out.println("Problem 3");
+    Scanner sc = new Scanner(System.in);
+    int n;
+    int counter1 = 0;
+
+    System.out.println("Enter a number between 0 and 50: ");
+    n = sc.nextInt();
+
+    if (n <= 0 || n >= 50)
+    {
+      System.out.println("error");
+
+    }
+    else if (n > 0 && n < 50)
+    {
+       for (int a = n; a <= 50; a++)
+    {
+      
+      if (counter1 % 5 == 4)
+      {
+      System.out.println(a + " ");
+      }
+      else
+      {
+        System.out.print(a + " ");
+      }
+      counter1++;
+
+    }
 
   }
+// Problem 4
+
+System.out.println("Problem 4");
+int num;
+System.out.println("Enter a number greater than 0");
+num = sc.nextInt();
+
+if (num <= 0)
+{
+  System.out.println("error");
+
+}
+else 
+{
+for (int b = num; b >= 0; b-- )
+{
+  if (b % 3 == 0)
+  {
+    System.out.print(b + " ");
+  }
+ 
+}
+}
+
+}
+
+
 }
